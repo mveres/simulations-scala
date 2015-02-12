9823a5ffb3f9dd01a51ce9d28b42de751cf7d0e4
Mihai Veres <veres85@yahoo.com>
2015-02-13 01:05:50 +0200

    some simulation implementation

--- src/main/scala/simulations/EpidemySimulator.scala
+++ src/main/scala/simulations/EpidemySimulator.scala
@@ -11,12 +11,39 @@ class EpidemySimulator extends Simulator {
     val roomRows: Int = 8
     val roomColumns: Int = 8
 
-    // to complete: additional parameters of simulation
+    val incubationTime = 6
+    val dieTime = 14
+    val immuneTime = 16
+    val healTime = 18
+
+    val prevalenceRate = 0.01
+    val transRate = 0.4
+    val dieRate = 0.25
   }
 
   import SimConfig._
 
-  val persons: List[Person] = List() // to complete: construct list of persons
+  agenda = (1 to 100).map(t => new WorkItem(t, () => persons.foreach(p => p.updateState))).toList
+
+  val persons: List[Person] = {
+    val ps = List.range(0, SimConfig.population).map(id => new Person(id))
+
+    def infectRandomPerson: Unit = {
+      val p = ps(randomBelow(population))
+      if(p.infected) infectRandomPerson
+      else p.infected = true
+    }
+
+    def infectInitialPersons: Unit = {
+      if (ps.count(p => p.infected) < population * prevalenceRate) {
+        infectRandomPerson
+        infectInitialPersons
+      }
+    }
+
+    infectInitialPersons
+    ps
+  }
 
   class Person (val id: Int) {
     var infected = false
@@ -24,12 +51,66 @@ class EpidemySimulator extends Simulator {
     var immune = false
     var dead = false
 
+    var shouldMove = false;
+
     // demonstrates random number generation
     var row: Int = randomBelow(roomRows)
     var col: Int = randomBelow(roomColumns)
 
-    //
-    // to complete with simulation logic
-    //
+    var deathChance = dieRate
+
+    var timeSinceInfected = 0
+
+    var timeSinceWantedToMove = 0
+    var timeToMove = Math.min(1, randomBelow(5))
+
+    def updateState = {
+      updateHealthState
+      move
+    }
+
+    def move: Unit = {
+      if (shouldMove)
+        shouldMove = false
+
+      timeSinceWantedToMove += 1
+      if (timeSinceWantedToMove == timeToMove) {
+        timeToMove = Math.min(1, randomBelow(5))
+        shouldMove = true
+        timeSinceWantedToMove = 0
+
+        val direction = randomBelow(3);
+        if (direction == 0) row = (row + 1) % roomRows
+        if (direction == 1) row = (row - 1 + roomRows) % roomRows
+        if (direction == 2) col = (col + 1) % roomColumns
+        if (direction == 1) col = (col - 1 + roomColumns) % roomColumns
+      }
+    }
+
+    def updateHealthState: Unit = {
+      if (infected)
+        timeSinceInfected += 1
+
+      if (timeSinceInfected == incubationTime)
+        sick = true
+
+      if (timeSinceInfected == dieTime && !dead)
+        dead = shouldDie
+
+      if (!dead) {
+        if (timeSinceInfected == immuneTime)
+          immune = true
+
+        if (timeSinceInfected == healTime) {
+          infected = false
+          sick = false
+          immune = false
+        }
+      }
+    }
+
+    def shouldDie: Boolean = {
+      randomBelow(100) < (deathChance * 100)
+    }
   }
 }
--- src/test/scala/simulations/EpidemySuite.scala
+++ src/test/scala/simulations/EpidemySuite.scala
@@ -14,7 +14,7 @@ class EpidemySuite extends FunSuite {
     val es = new EpidemySimulator
     val numInfected = es.persons.count(_.infected)
 
-    assert(numInfected == es.SimConfig.population * prevalenceRate,
+    assert(numInfected === es.SimConfig.population * prevalenceRate,
       "prevalence rate should be 0.01"
       )
   }
@@ -32,7 +32,7 @@ class EpidemySuite extends FunSuite {
 
     val testDays = 100
 
-    while(!es.agenda.isEmpty && es.agenda.head.time < testDays){
+    while(es.agenda.nonEmpty && es.agenda.head.time < testDays){
       es.next
 
       assert(chosenOne.dead == true, "Dead person should keep dead state")
@@ -55,33 +55,68 @@ class EpidemySuite extends FunSuite {
     val transRate = 0.4
     val dieRate = 0.25
 
-    val infectedPerson = (es.persons.find{_.infected}).get
+    val infectedPerson = es.persons.find{_.infected}.get
 
     //before incubation time
     while(es.agenda.head.time < incubationTime){
-      assert(infectedPerson.infected == true, "Infected person keeps infected in 6 days")
-    	assert(infectedPerson.sick == false, "Infected person does not get sick in 6 days")
-    	assert(infectedPerson.immune == false, "Infected person cannot become immune in 6 days")
-    	assert(infectedPerson.dead == false, "Infected person does not die in 6 days")
+      assert(infectedPerson.infected === true, "Infected person keeps infected in 6 days")
+    	assert(infectedPerson.sick === false, "Infected person does not get sick in 6 days")
+    	assert(infectedPerson.immune === false, "Infected person cannot become immune in 6 days")
+    	assert(infectedPerson.dead === false, "Infected person does not die in 6 days")
     	es.next
     }
 
     //incubation time has passed, there should be an event for getting sick
-    assert(es.agenda.head.time == incubationTime, "You should set a 'sick' event after incubation time")
+    assert(es.agenda.head.time === incubationTime, "You should set a 'sick' event after incubation time")
     while(es.agenda.head.time == incubationTime) es.next
-    assert(infectedPerson.sick == true, "Infected person should become sick after 6 days")
+    assert(infectedPerson.sick === true, "Infected person should become sick after 6 days")
 
+    infectedPerson.deathChance = 1 //increase death chance to 100% for testing purposes
     //wait for dieTime
     while(es.agenda.head.time < dieTime){
-    	assert(infectedPerson.infected == true, "Sick person keeps infected")
-    	assert(infectedPerson.sick == true, "Sick person keeps sick before turning immune")
-    	assert(infectedPerson.immune == false, "Sick person is not immune")
-    	assert(infectedPerson.dead == false, "Sick person does not die before 14 infected days")
+    	assert(infectedPerson.infected === true, "Sick person keeps infected")
+    	assert(infectedPerson.sick === true, "Sick person keeps sick before turning immune")
+    	assert(infectedPerson.immune === false, "Sick person is not immune")
+    	assert(infectedPerson.dead === false, "Sick person does not die before 14 infected days")
     	es.next
     }
 
-    assert(es.agenda.head.time == dieTime, "You should set a 'die' event (decides with a probability 25% whether the person dies) after 14 days")
+    assert(es.agenda.head.time === dieTime, "You should set a 'die' event (decides with a probability 25% whether the person dies) after 14 days")
     while(es.agenda.head.time == dieTime) es.next
+
+    assert(infectedPerson.dead === true, "Sick person should die after 14 dies")
+  }
+
+  test("life cycle - lucky person"){
+    val es = new EpidemySimulator
+
+    val incubationTime = 6
+    val dieTime = 14
+    val immuneTime = 16
+    val healTime = 18
+
+    val prevalenceRate = 0.01
+    val transRate = 0.4
+    val dieRate = 0.25
+
+    val infectedPerson = es.persons.find{_.infected}.get
+
+    infectedPerson.deathChance = 0 //decrease death chance to 0% for testing purposes
+    //wait for dieTime
+    while(es.agenda.head.time <= dieTime) es.next
+
+    assert(infectedPerson.dead === false, "Lucky person does not die after 14 days")
+
+    while(es.agenda.head.time <= immuneTime) es.next
+
+    assert(infectedPerson.immune === true, "After 16 days a person should be immune")
+
+    while(es.agenda.head.time <= healTime) es.next
+
+    assert(infectedPerson.infected === false, "After 18 days a person should be healthy")
+    assert(infectedPerson.sick === false, "After 18 days a person should be healthy")
+    assert(infectedPerson.dead === false, "After 18 days a person should be healthy")
+    assert(infectedPerson.immune === false, "After 18 days a person should be healthy and not immune any more")
   }
 
 
@@ -97,5 +132,42 @@ class EpidemySuite extends FunSuite {
       infectedTimes = infectedTimes + (if(healthyPerson.infected) 1 else 0)
 	  }
 	  assert(infectedTimes > 0, "A person should get infected according to the transmissibility rate when he moves into a room with an infectious person")
+  }
+
+  test("a person should want to move after max 5 days"){
+    val es = new EpidemySimulator
+    val person = es.persons.head
+
+    assert(person.shouldMove === false)
+
+    var daysPast = 0
+    while (!person.shouldMove && daysPast <= 5) {
+      daysPast += 1
+      es.next
+    }
+
+    assert(person.shouldMove === true, "A person should want to move in max 5 days")
+
+    es.next
+    assert(person.shouldMove === false, "A person should not want to move in first day after he moved")
+  }
+
+  test ("a person moves to one neighbour room when nobody is sick or dead"){
+
+    val es = new EpidemySimulator
+    val person = es.persons.head
+
+    val row = person.row
+    val col = person.col
+
+    var daysPast = 0
+    while (!person.shouldMove && daysPast <= 5) {
+      daysPast += 1
+      es.next
+    }
+
+    val dx = Math.abs(row - person.row)
+    val dy = Math.abs(col - person.col)
+    assert(dx + dy === 1, "person should have moved to a neighbour cell")
   }
 }
